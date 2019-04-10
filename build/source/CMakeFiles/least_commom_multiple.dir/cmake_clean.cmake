985f2513c83fdaa89c8e40a7ebe7e077f7ce3f68
Kai Lorenz <kai.lorenz@uni-weimar.de>
2019-04-10 14:38:12 +0200

    gitignore fix
    Former-commit-id: cae21b750b615e44fa6578989ef7c60766b5d678

---
+++ build/source/CMakeFiles/least_commom_multiple.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/least_commom_multiple.dir/lcm.cpp.o"
+  "least_commom_multiple.pdb"
+  "least_commom_multiple"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/least_commom_multiple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
